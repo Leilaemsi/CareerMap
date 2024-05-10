3ebd527f81741ae03e088a6e7048e011775f2a0e
Leilaemsi <emsileila@gmail.com>
2024-05-10 14:31:51 +0200

    Realisition du calcule de personnalité à travers les réponses Realisition du calcule de personnalité à travers les réponses

--- Back-end/Careermap/src/main/java/com/example/careermap/service/impl/UtilisateurImpl.java
+++ Back-end/Careermap/src/main/java/com/example/careermap/service/impl/UtilisateurImpl.java
@@ -89,5 +89,88 @@ public class UtilisateurImpl implements UtilisateurService {
 
 	
    
+	@Override
+	public String calculatePersonality(Long userId, Long testId) {
+		        List<String> responses = reponseRepository.findByMatriculeAndIdTest(userId, testId);
+		        int introversionSum = 0, sensingSum = 0, thinkingSum = 0, judgingSum = 0;
+
+		        for (int i = 0; i < responses.size(); i++) {
+		            int percent = Integer.parseInt(responses.get(i).trim());
+
+		            switch (i % 4) {
+		                case 0:
+		                case 4:
+		                case 8:
+		                case 12:
+		                case 16:
+		                case 20:
+		                case 24:
+		                    introversionSum += percent;
+		                    break;
+		                case 1:
+		                case 5:
+		                case 9:
+		                case 13:
+		                case 17:
+		                case 21:
+		                case 25:
+		                    sensingSum += percent;
+		                    break;
+		                case 2:
+		                case 6:
+		                case 10:
+		                case 14:
+		                case 18:
+		                case 22:
+		                case 26:
+		                    thinkingSum += percent;
+		                    break;
+		                case 3:
+		                case 7:
+		                case 11:
+		                case 15:
+		                case 19:
+		                case 23:
+		                case 27:
+		                    judgingSum += percent;
+		                    break;
+		            }
+		        }
+
+		        int totalResponses = responses.size() / 4;
+		        int introversionPercentage = introversionSum / totalResponses;
+		        int sensingPercentage = sensingSum / totalResponses;
+		        int thinkingPercentage = thinkingSum / totalResponses;
+		        int judgingPercentage = judgingSum / totalResponses;
+
+		        String personality = "";
+		        if (introversionPercentage > 50) {
+		            personality += "I";
+		        } else {
+		            personality += "E";
+		        }
+
+		        if (sensingPercentage > 50) {
+		            personality += "S";
+		        } else {
+		            personality += "N";
+		        }
+
+		        if (thinkingPercentage > 50) {
+		            personality += "T";
+		        } else {
+		            personality += "F";
+		        }
+
+		        if (judgingPercentage > 50) {
+		            personality += "J";
+		        } else {
+		            personality += "P";
+		        }
+
+		      
+		        return personality;
+		    
+	}
  
 }
