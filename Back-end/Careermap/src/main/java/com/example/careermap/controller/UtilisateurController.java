0c47dfb76fbcd2b4bbba3d11ee30cd78cc87f410
oumaimadgn <oumaima.dagoun@gmail.com>
2024-05-13 21:31:37 +0100

    modifying controller user

--- Back-end/Careermap/src/main/java/com/example/careermap/controller/UtilisateurController.java
+++ Back-end/Careermap/src/main/java/com/example/careermap/controller/UtilisateurController.java
@@ -1,34 +1,22 @@
 package com.example.careermap.controller;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-=======
-
-import javax.annotation.security.PermitAll;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
->>>>>>> f962d6087840983d5858ed32adca6fff306f4c0f
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
 import com.example.careermap.dto.AuthentificationDTO;
 import com.example.careermap.dto.PersonalityJobDTO;
 import com.example.careermap.dto.QuestionDTO;
 import com.example.careermap.dto.ResultatDTO;
 import com.example.careermap.dto.TestDTO;
 import com.example.careermap.dto.UtilisateurDTO;
-import com.example.careermap.payload.AuthentificationMessage;
 import com.example.careermap.repository.JobsRepo;
 import com.example.careermap.service.UtilisateurService;
 import com.example.careermap.entity.Reponse;
@@ -36,44 +24,32 @@ import com.example.careermap.entity.Jobs;
 import com.example.careermap.entity.Resultat;
 import com.example.careermap.entity.Test;
 import com.example.careermap.entity.Utilisateur;
-=======
-
-import com.example.careermap.dto.AuthentificationDTO;
-import com.example.careermap.dto.QuestionDTO;
-import com.example.careermap.dto.TestDTO;
-import com.example.careermap.dto.UtilisateurDTO;
-import com.example.careermap.payload.AuthentificationMessage;
-import com.example.careermap.service.UtilisateurService;
-import com.example.careermap.entity.Reponse;
-import com.example.careermap.entity.Resultat;
->>>>>>> f962d6087840983d5858ed32adca6fff306f4c0f
 
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/utilisateur")
 public class UtilisateurController {
-<<<<<<< HEAD
 	
 	@Autowired
 	private JobsRepo jobsRepo;
-
-=======
->>>>>>> f962d6087840983d5858ed32adca6fff306f4c0f
 
     @Autowired
     private UtilisateurService utilisateurService;
 
     @PostMapping(path = "/save")
-    public ResponseEntity<String> saveUtilisateur(@RequestBody UtilisateurDTO utilisateurDTO) {
-        String id = utilisateurService.addUtilisateur(utilisateurDTO);
-        return ResponseEntity.ok(id);
+    public ResponseEntity<Utilisateur> saveUtilisateur(@RequestBody UtilisateurDTO utilisateurDTO) {
+        Utilisateur id = utilisateurService.addUtilisateur(utilisateurDTO);
+      //  return ResponseEntity.ok(id);
+       return  ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
+
     @PostMapping(path = "/login")
-    public ResponseEntity<AuthentificationMessage> authentificationUtilisateur(@RequestBody AuthentificationDTO authentificationDTO) {
-        AuthentificationMessage authentificationMessage = utilisateurService.authentificationUtilisateur(authentificationDTO);
-        return ResponseEntity.ok(authentificationMessage);
+    public ResponseEntity<Map<String, Object>> authentificationplus(@RequestBody AuthentificationDTO authentificationDTO) {
+        Map<String, Object> response = utilisateurService.mylogin(authentificationDTO);
+        HttpStatus httpStatus = (Boolean) response.get("status") ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(httpStatus).body(response);
     }
 
     @PostMapping(path = "/fetchQuestions")
@@ -99,17 +75,10 @@ public class UtilisateurController {
     }
     
     @PostMapping("/resultat")
-<<<<<<< HEAD
     public ResponseEntity<String> createResultat(@RequestBody ResultatDTO resultatDTO) {
         try {
             Resultat resultat = convertToEntity(resultatDTO);
             boolean saved = utilisateurService.addResultat(resultat);
-=======
-    public ResponseEntity<String> createResultat(@RequestBody Resultat resultat ) {
-        try {
-            boolean saved = utilisateurService.addResultat(resultat);
-            utilisateurService.addResultat(resultat);
->>>>>>> f962d6087840983d5858ed32adca6fff306f4c0f
             if (saved) {
                 return new ResponseEntity<>("Resultat created successfully", HttpStatus.CREATED);
             } else {
@@ -120,8 +89,24 @@ public class UtilisateurController {
         }
     }
 
-<<<<<<< HEAD
- 
+    private Resultat convertToEntity(ResultatDTO resultatDTO) {
+    	System.out.println("Leilaaa2");
+        Resultat resultat = new Resultat();
+        resultat.setId_resultat(resultatDTO.getIdResultat());
+        resultat.setDate(resultatDTO.getDate());
+        Map<String, Object> perso = utilisateurService.calculatePersonality(resultatDTO.getFkUtilisateurId(),resultatDTO.getFkTestId());
+        String personality = (String) perso.get("personality");
+        System.out.println("Leilaaa2");
+        resultat.setPersonnalite(personality);
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setMatricule(resultatDTO.getFkUtilisateurId());   
+        resultat.setFk_utilisateur(utilisateur);
+        Test test = new Test();
+        test.setId_test(resultatDTO.getFkTestId());   
+        resultat.setFk_test(test);
+
+        return resultat;
+    }
 
 
 
@@ -152,11 +137,5 @@ public class UtilisateurController {
    
 
     
-=======
-    @RequestMapping("/personality/{userId}/{testId}")
-    public String getPersonality(@PathVariable("userId") Long userId, @PathVariable("testId") Long testId) {
-		return  utilisateurService.calculatePersonality(userId, testId);
-    }
->>>>>>> f962d6087840983d5858ed32adca6fff306f4c0f
 
 }
