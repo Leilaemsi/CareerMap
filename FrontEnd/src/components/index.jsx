8a3efa964f84d65f9637340fd8079f890c913473
AYA Nor Elyakine <121770088+norelyakine@users.noreply.github.com>
2024-05-23 13:10:46 +0100

    Delete FrontEnd directory

--- FrontEnd/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Img } from "./Img";
-import { Button } from "./Button";
-import { Text } from "./Text";
-import { Heading } from "./Heading"; 
-import { Input } from "./Input";
-export { Img, Button, Text, Heading, Input };
